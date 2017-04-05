a9c618e35e26abaf41d9050390c1efb9f952e2d6
MANGALAGANESH BALASUBRAMANIAN <bmangalaganesh@au1.ibm.com>
2017-04-05 15:24:25 +1000

    removed the unused import (import org.assertj.core.util)

--- src/main/java/com/ibm/aaspire/poc/web/MemberController.java
+++ src/main/java/com/ibm/aaspire/poc/web/MemberController.java
@@ -3,7 +3,6 @@ package com.ibm.aaspire.poc.web;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.assertj.core.util.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
