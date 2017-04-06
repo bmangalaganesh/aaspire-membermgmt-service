ef6fa3d9b50e4a061ffbbfca9bf19a7df14266b9
Nikolay Grozev <nikolayg@au1.ibm.com>
2017-04-06 16:21:22 +1000

    Added search in the API

--- src/main/java/com/ibm/aaspire/poc/services/MemberService.java
+++ src/main/java/com/ibm/aaspire/poc/services/MemberService.java
@@ -1,5 +1,14 @@
 package com.ibm.aaspire.poc.services;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +20,9 @@ public class MemberService {
 
 	@Autowired
 	private MemberRepository repo;
+	
+	@PersistenceContext
+  	private EntityManager em;
 
 	/*
 	 * Member aMember = new Member(); aMember.setter
@@ -30,5 +42,34 @@ public class MemberService {
 	public Member findByGivenNameAndLastName(String givenName, String lastName) {
 		return repo.findByGivenNameAndSurname(givenName, lastName);
 	}
+	
 
+	public Iterable<Member> search(String queryTxt) {
+		String cleanQueryTxt = queryTxt == null? "" : queryTxt.trim().toLowerCase();
+		
+		if(cleanQueryTxt.isEmpty()) {
+			return fetchAllMembers();
+		} else {
+			List<String> terms = Arrays.asList(cleanQueryTxt.split("\\s+"));
+			List<String> subqueries = IntStream.range(0, terms.size()).mapToObj(i -> termSubquery(terms.get(i), i)).collect(Collectors.toList());
+			
+			String queryExpression = String.join(" AND ", subqueries);
+			TypedQuery<Member> query = em.createQuery("select m from Member m where " + queryExpression, Member.class);
+			IntStream.range(0, terms.size()).forEach(i -> query.setParameter("term" + i, "%"+ terms.get(i) + "%"));
+			
+			return query.getResultList();	
+		}
+	}
+	
+	private String termSubquery(String term, int i) {
+		return String.format("LOWER(CONCAT(m.givenName, "
+				+ "m.surname, "
+				+ "m.email, "
+				+ "m.phoneNumber, "
+				+ "m.address.line1, "
+				+ "m.address.line2, "
+				+ "m.address.postCode, "
+				+ "m.address.suburb, "
+				+ "m.address.state)) LIKE :%s", "term" + i);
+	}
 }
--- src/main/java/com/ibm/aaspire/poc/web/MemberController.java
+++ src/main/java/com/ibm/aaspire/poc/web/MemberController.java
@@ -1,10 +1,8 @@
 package com.ibm.aaspire.poc.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.aaspire.poc.entities.Member;
@@ -15,26 +13,10 @@ public class MemberController {
 
 	@Autowired
 	MemberService memberService;
+	
 
 	@GetMapping("/members")
-	public Iterable<Member> getMembers() {
-
-		List<Member> list = new ArrayList<>();
-
-		return memberService.fetchAllMembers();
-		// return Lists.newArrayList(repo.findAll());
-
+	public Iterable<Member> getMembers(@RequestParam(value = "search", required = false) String search) {
+		return memberService.search(search);
 	}
-
-	@GetMapping("/members/testgivenname")
-	public Member findMemberBasedOnName() {
-
-		String firstName = "Grigor";
-		String lastName = "Dimitrov";
-
-		return memberService.findByGivenNameAndLastName(firstName, lastName);
-		// return Lists.newArrayList(repo.findAll());
-
-	}
-
 }
