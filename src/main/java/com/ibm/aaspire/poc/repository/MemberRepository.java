e888a084394002b2253ed0ff86c31115a1103431
MANGALAGANESH BALASUBRAMANIAN <bmangalaganesh@au1.ibm.com>
2017-04-06 15:08:22 +1000

    additional methods to perform search on Member repository

--- src/main/java/com/ibm/aaspire/poc/repository/MemberRepository.java
+++ src/main/java/com/ibm/aaspire/poc/repository/MemberRepository.java
@@ -6,4 +6,8 @@ import com.ibm.aaspire.poc.entities.Member;
 
 public interface MemberRepository extends CrudRepository<Member, String> {
 
+	public Member findByGivenNameAndSurname(String givenName, String surname);
+	
+	public Iterable<Member> findByGivenNameIgnoreCaseContaining(String givenName);
+
 }
