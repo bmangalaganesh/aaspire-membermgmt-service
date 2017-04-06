153d8f7ec286e08d464442683090157148c28d4f
MANGALAGANESH BALASUBRAMANIAN <bmangalaganesh@au1.ibm.com>
2017-04-06 12:12:11 +1000

    added code to test the vcap property file injection issue

--- src/main/java/com/ibm/aaspire/poc/config/AppConfig.java
+++ src/main/java/com/ibm/aaspire/poc/config/AppConfig.java
@@ -1,9 +1,13 @@
 package com.ibm.aaspire.poc.config;
 
+import java.util.Map;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -24,6 +28,12 @@ public class AppConfig {
 	@Bean
 	public DataSource dataSource() {
 
+		String vcap = System.getenv("VCAP_SERVICES");
+
+		System.err.println("VCAP Services:" + vcap);
+
+		JsonParser parser = JsonParserFactory.getJsonParser();
+		Map<String, Object> jsonMap = parser.parseMap(vcap);
 		// Do the parsing activity here..
 
 		String[] parts = dbUrl.split("(://|@)");
