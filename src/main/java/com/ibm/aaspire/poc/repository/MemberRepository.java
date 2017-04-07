7309fcb52d2247ed021f30ba0563279d5066ecf1
Nikolay Grozev <nikolayg@au1.ibm.com>
2017-04-07 23:31:04 +1000

    Removed obsolete query methods.

--- src/main/java/com/ibm/aaspire/poc/repository/MemberRepository.java
+++ src/main/java/com/ibm/aaspire/poc/repository/MemberRepository.java
@@ -5,9 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import com.ibm.aaspire.poc.entities.Member;
 
 public interface MemberRepository extends CrudRepository<Member, String> {
-
-	public Member findByGivenNameAndSurname(String givenName, String surname);
-	
-	public Iterable<Member> findByGivenNameIgnoreCaseContaining(String givenName);
-
 }
