288b281715a0b61733097b96a9834fed7b3213fa
MANGALAGANESH BALASUBRAMANIAN <bmangalaganesh@au1.ibm.com>
2017-04-07 10:47:27 +1000

    added reasonable fake data for demo purposes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,33 @@
-INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('101','Australia', '60 City Road','Southgate', '3006', 'VIC', 'South Bank');
-INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('102','Australia', '50 City Road','Southgate', '3006', 'VIC', 'South Bank');
-INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('103','Australia', '40 City Road','Southgate', '3006', 'VIC', 'South Bank');
-INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('104','Australia', 'Langham Hotel','Southgate', '3006', 'VIC', 'South Bank');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('101','Australia', '68 Drummond Street',' ', '3053', 'VIC', 'Carlton');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('102','Australia', 'Kay Street',' ', '3053', 'VIC', 'Carlton');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('103','Australia', '118 Westgarth Street',' ', '3070', 'VIC', 'Northcote');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('104','Australia', '7 Sherwin Street',' ', '3031', 'VIC', 'Kensington');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('105','Australia', '10 Dartford Street',' ', '3031', 'VIC', 'Flemington');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('106','Australia', '369 High Street',' ', '3101', 'VIC', 'Kew');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('107','Australia', '336 Union Road',' ', '3103', 'VIC', 'Balwyn');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('108','Australia', '4 Lynch Court',' ', '3103', 'VIC', 'Balwyn');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('109','Australia', '49 Metung Street',' ', '3103', 'VIC', 'Balwyn');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('110','Australia', '68 Mary Street',' ', '3101', 'VIC', 'Kew');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('111','Australia', '9 Belfairs Street',' ', '4109', 'QLD', 'Robertson');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('112','Australia', '4 Kidd St',' ', '4109', 'QLD', 'Robertson');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('113','Australia', '38 Keats Street',' ', '4109', 'QLD', 'Sunnybank');
+INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('114','Australia', '481 Beenleigh Road',' ', '4109', 'QLD', 'Sunnybank');
 
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('201', '1991-01-01','Standard','abc@gmail.com','Roger','12345678','ACTIVE','Federer','Mr','101');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('202', '1991-01-01','Standard','abc@gmail.com','Novak','12345678','ACTIVE','Djokovic','Mr','102');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('203', '1991-01-01','Standard','abc@gmail.com','Rafael','12345678','ACTIVE','Nadal','Mr','103');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('204', '1991-01-01','Standard','abc@gmail.com','Grigor','12345678','ACTIVE','Dimitrov','Mr','104');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('205', '1991-01-01','Standard','abc@gmail.com','Maria','12345678','ACTIVE','Sharapova','Ms','104');
+
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('201', '1991-01-01','Standard','minerva@bartoletti.net','Eloy','12345678','ACTIVE','Abbott','Mr','101');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('202', '1992-01-01','Standard','randal@murray.co','Jorge','12345678','ACTIVE','Torp','Mr','102');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('203', '1993-01-01','Standard','prince@binspagac.com','Arnold','12345678','ACTIVE','Kling','Mr','103');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('204', '1994-01-01','Standard','helena_mueller@bayer.org','Glen','12345678','ACTIVE','Bergstrom','Mr','104');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('205', '1995-01-01','Standard','verona_huel@orn.biz','Sylvester','12345678','ACTIVE','Boyle','Mr','105');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('206', '1996-01-01','Standard','elwin_herman@reichelhamill.io','Sammy','12345678','ACTIVE','Doyle','Mr','110');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('207', '1997-01-01','Standard','brad.bins@lynchkaulke.info','Zoila','12345678','ACTIVE','Cormier','Ms','111');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('208', '1998-01-01','Standard','alanis@casperbreitenberg.io','Alessandro','12345678','ACTIVE','Mante','Ms','112');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('209', '1990-01-01','Standard','albina.dickinson@sanfordkozey.info','Dayna','12345678','ACTIVE','Grady','Ms','113');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('210', '1991-02-02','Standard','ludie_hauck@mann.info','Lennie','12345678','ACTIVE','Keebler','Ms','114');
+
+
+
+
 
 
 
