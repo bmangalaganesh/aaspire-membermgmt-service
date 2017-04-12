46b8dc46ba0ad58a42e952b3e209791473123fe7
MANGALAGANESH BALASUBRAMANIAN <bmangalaganesh@au1.ibm.com>
2017-04-12 11:40:34 +1000

    test data to supported the updated member model

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,17 +14,45 @@ INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb
 INSERT INTO ADDRESS("id","country","line1","line2","post_code", "state", "suburb") VALUES('114','Australia', '481 Beenleigh Road',' ', '4109', 'QLD', 'Sunnybank');
 
 
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('201', '1991-01-01','Standard','minerva@bartoletti.net','Eloy','12345678','ACTIVE','Abbott','Mr','101');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('202', '1992-01-01','Standard','randal@murray.co','Jorge','12345678','ACTIVE','Torp','Mr','102');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('203', '1993-01-01','Standard','prince@binspagac.com','Arnold','12345678','ACTIVE','Kling','Mr','103');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('204', '1994-01-01','Standard','helena_mueller@bayer.org','Glen','12345678','ACTIVE','Bergstrom','Mr','104');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('205', '1995-01-01','Standard','verona_huel@orn.biz','Sylvester','12345678','ACTIVE','Boyle','Mr','105');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('206', '1996-01-01','Standard','elwin_herman@reichelhamill.io','Sammy','12345678','ACTIVE','Doyle','Mr','110');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('207', '1997-01-01','Standard','brad.bins@lynchkaulke.info','Zoila','12345678','ACTIVE','Cormier','Ms','111');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('208', '1998-01-01','Standard','alanis@casperbreitenberg.io','Alessandro','12345678','ACTIVE','Mante','Ms','112');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('209', '1990-01-01','Standard','albina.dickinson@sanfordkozey.info','Dayna','12345678','ACTIVE','Grady','Ms','113');
-INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id") VALUES ('210', '1991-02-02','Standard','ludie_hauck@mann.info','Lennie','12345678','ACTIVE','Keebler','Ms','114');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id","last_employer") VALUES ('201', '1991-01-01','Standard','minerva@bartoletti.net','Eloy','12345678','ACTIVE','Abbott','Mr','101','PWC');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id","last_employer") VALUES ('202', '1992-01-01','Standard','randal@murray.co','Jorge','12345678','ACTIVE','Torp','Mr','102','Melbourne Water');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id","last_employer") VALUES ('203', '1993-01-01','Standard','prince@binspagac.com','Arnold','12345678','ACTIVE','Kling','Mr','103','Australian Super');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id","last_employer") VALUES ('204', '1994-01-01','Standard','helena_mueller@bayer.org','Glen','12345678','ACTIVE','Bergstrom','Mr','104','Richmond Tigers');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id","last_employer") VALUES ('205', '1995-01-01','Standard','verona_huel@orn.biz','Sylvester','12345678','ACTIVE','Boyle','Mr','105','Melbourne Uni');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id","last_employer") VALUES ('206', '1996-01-01','Standard','elwin_herman@reichelhamill.io','Sammy','12345678','ACTIVE','Doyle','Mr','110','United Energy');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id","last_employer") VALUES ('207', '1997-01-01','Standard','brad.bins@lynchkaulke.info','Zoila','12345678','ACTIVE','Cormier','Ms','111','AAPT');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id","last_employer") VALUES ('208', '1998-01-01','Standard','alanis@casperbreitenberg.io','Alessandro','12345678','ACTIVE','Mante','Ms','112','Suncorp');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id","last_employer") VALUES ('209', '1990-01-01','Standard','albina.dickinson@sanfordkozey.info','Dayna','12345678','ACTIVE','Grady','Ms','113','Bluewolf');
+INSERT INTO MEMBER ("id","date_of_birth","plan","email","given_name","phone_number","status","surname","title","address_id","last_employer") VALUES ('210', '1991-02-02','Standard','ludie_hauck@mann.info','Lennie','12345678','ACTIVE','Keebler','Ms','114','Salesforce');
 
+
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('301', 'LICENSE','3102876208','VIC','1991-02-09','2021-02-02','201');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('302', 'LICENSE','3352947972','VIC','1992-02-08','2022-02-02','202');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('303', 'LICENSE','1020531875','VIC','1993-02-07','2023-02-02','203');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('304', 'LICENSE','5467198322','VIC','1994-02-06','2024-02-02','204');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('305', 'LICENSE','6290530115','VIC','1995-02-05','2025-02-02','205');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('306', 'LICENSE','5592755795','VIC','1996-02-04','2026-02-02','206');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('307', 'LICENSE','8880481027','QLD','1997-02-02','2027-02-02','207');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('308', 'LICENSE','2060041410','QLD','1998-02-02','2028-02-02','208');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('309', 'LICENSE','2644039823','QLD','1999-02-02','2029-02-02','209');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('310', 'LICENSE','5356560542','QLD','1991-03-02','2021-03-02','210');
+
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('311', 'PASSPORT','6419916706','VIC','1991-04-02','2021-04-01','201');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('312', 'PASSPORT','5261282598','VIC','1991-05-02','2021-05-01','202');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('313', 'PASSPORT','7836904477','VIC','1991-06-02','2021-06-01','203');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('314', 'PASSPORT','8341816491','VIC','1991-07-02','2021-07-01','204');
+INSERT INTO IDENTITYINFO ("id","type","document_number","issuer","issue_date","expiry_date","member_id") VALUES ('315', 'PASSPORT','5005820711','VIC','1991-08-02','2021-08-01','205');
+
+
+
+
+
+
+
+
+
+
+ 
 
 
 
